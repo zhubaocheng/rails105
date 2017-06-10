d5927e437edcf30464ef182b6cf75c25eb2e1ba2
zhubaocheng <imzhubaocheng@gmail.com>
2017-06-10 23:14:40 +0800

    create groups index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,7 @@
 class WelcomeController < ApplicationController
+  def index
+    flash[:notice] = "Good Morning!"
+    flash[:alert] = "Good Night! It's time to sleep."
+    flash[:warning] = "This is a warning information."
+  end
 end
