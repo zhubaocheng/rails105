46200799b9a2d478e9ceddc2f1e51a8ba51d35e4
zhubaocheng <imzhubaocheng@gmail.com>
2017-06-12 17:15:01 +0800

    必须是创建者才能进行编辑更新删除

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @groups = Group.all
@@ -15,6 +15,11 @@ class GroupsController < ApplicationController
 
   def edit
     @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path
+      flash[:alert] = "你没有权限，无法操作！"
+    end
   end
 
   def create
@@ -31,6 +36,12 @@ class GroupsController < ApplicationController
 
   def update
     @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path
+      flash[:alert] = "你没有权限，无法操作！"
+    end
+
     if @group.update(group_params)
       redirect_to groups_path
       flash[:notice]= "编辑成功！"
@@ -41,6 +52,10 @@ class GroupsController < ApplicationController
 
   def destroy
     @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path
+      flash[:alert] = "你没有权限，无法操作！"
 
     @group.destroy
     redirect_to groups_path
